c53d9c2ea3f6738ead8cbadca2eb3c2b98cb66b1
你见唔到我 <403482009@qq.com>
2018-06-11 16:50:20 +0800

    更新测试样例

--- SYGLKit/ViewController.m
+++ SYGLKit/ViewController.m
@@ -11,13 +11,25 @@
 #import "SYGLImage.h"
 #import "SYGLFilter.h"
 #import "SYGLVideoCamera.h"
+#import "SYGLBlendFilter.h"
 
-@interface ViewController ()
+@interface ViewController ()<SYGLVideoCameraDelegate>
 
 @property (nonatomic,strong)SYGLView *presentView;
 
+@property (nonatomic) CGRect presentViewRect;
+
 @property (nonatomic,strong)SYGLVideoCamera *camera;
 
+@property (nonatomic,strong)SYGLImage *firstImageTexture;
+
+@property (nonatomic,strong)SYGLImage *secondImageTexture;
+
+@property (nonatomic,strong)SYGLBlendFilter *twoInputFilters;
+
+@property (nonatomic,strong)SYGLFilter *firstEmptyFilter;
+
+@property (nonatomic,strong)SYGLFilter *secondEmptyFilter;
 @end
 
 @implementation ViewController
@@ -25,34 +37,174 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    _presentView = [[SYGLView alloc]initWithFrame:self.view.frame];
+    [self configDisplayView];
+    
+    [self configImageSource];
+    
+    [self configEmptyFilter];
+    
+    [self configCameraSource];
+    
+    [self configTwoInputsFilter];
+    
+    [self configFilterChain];
 
-    //屏幕居中自适应模式
-    _presentView.contentMode = SYGLFrameBufferObjectContentModeResizeAspect;
-    
-    [self.view addSubview:_presentView];
-    
-    /*
-     //测试图片渲染
-    SYGLImage *imageTexture = [[SYGLImage alloc]initWithUIImage:[UIImage imageNamed:@"backGroundImageSource.jpg"]];
-
-    [imageTexture addConsumer:_presentView];
-
-    [imageTexture produceAtTime:kCMTimeInvalid];
-    */
-    
-    _camera = [[SYGLVideoCamera alloc]initWithCameraPosition:AVCaptureDevicePositionBack sessionPreset:AVCaptureSessionPresetPhoto];
-//    (width = 1080, height = 1440)
-
-    
-    [_camera addConsumer:_presentView];
-    
-    [_camera startRunning];
     
 }
 
 
+-(void)configImageSource{
+    //测试图片渲染
+//    _imageTexture = [[SYGLImage alloc]initWithUIImage:[UIImage imageNamed:@"backGroundImageSource.jpg"]];
+    _firstImageTexture = [[SYGLImage alloc]initWithUIImage:[UIImage imageNamed:@"backGroundImageSource.jpg"]];
+    
+    _secondImageTexture =  [[SYGLImage alloc]initWithUIImage:[UIImage imageNamed:@"smallSizeImageSource.jpg"]];
+    
+    _presentViewRect = AVMakeRectWithAspectRatioInsideRect(_firstImageTexture.size, self.view.frame);
+    
+}
+
+-(void)configCameraSource{
+    _camera = [[SYGLVideoCamera alloc]initWithCameraPosition:AVCaptureDevicePositionBack sessionPreset:AVCaptureSessionPresetPhoto];
+    //    (width = 1080, height = 1440)
+    
+    _camera.delegate = self;
+}
+
+-(void)configTwoInputsFilter{
+    //Two Inputs
+    _twoInputFilters = [[SYGLBlendFilter alloc]initWithContentSize:_presentView.contentSize];
+    _twoInputFilters.opacity = 0.5f;
+}
+
+-(void)configEmptyFilter{
+    //Empty Filter
+    _firstEmptyFilter = [[SYGLFilter alloc]initWithContentSize:_presentView.contentSize];
+    
+    _firstEmptyFilter.outputFrame = CGRectMake(0, 0, _presentView.contentSize.width, _presentView.contentSize.height);
+    
+    _secondEmptyFilter = [[SYGLFilter alloc]initWithContentSize:_presentView.contentSize];
+    
+    _secondEmptyFilter.outputFrame = CGRectMake(0, 0, _presentView.contentSize.width, _presentView.contentSize.height);
+}
+
+-(void)configDisplayView{
+    _presentView = [[SYGLView alloc]initWithFrame:self.view.frame];
+    
+    //屏幕居中自适应模式
+//    _presentView.contentMode = SYGLFrameBufferObjectContentModeResizeAspect;
+    _presentView.contentMode = SYGLFrameBufferObjectContentModeNormal;
+    
+    [self.view addSubview:_presentView];
+}
+
+-(void)configFilterChain{
+//    [_firstImageTexture addConsumer:_firstEmptyFilter];
+//
+//    [_firstEmptyFilter addConsumer:_presentView];
+//
+//    [_firstImageTexture produceAtTime:kCMTimeInvalid];
+//
+//
+//    [_secondEmptyFilter addConsumer:_presentView];
+//
+//    [_secondEmptyFilter produceAtTime:kCMTimeInvalid];
+//
+//
+//    [_secondImageTexture addConsumer:_firstEmptyFilter];
+//
+//    [_firstEmptyFilter addConsumer:_presentView];
+//
+//    [_secondImageTexture produceAtTime:kCMTimeInvalid];
+    
+    
+    
+//    [_secondImageTexture addConsumer:_firstEmptyFilter];
+//
+//    _firstEmptyFilter.contentSize = CGSizeMake(640, 640);
+//
+//    _firstImageTexture.outputFrame = CGRectMake(0, 0, _twoInputFilters.contentSize.width, _twoInputFilters.contentSize.height);
+//
+//    [_firstEmptyFilter addConsumer:_presentView];
+//
+//    [_secondImageTexture produceAtTime:kCMTimeInvalid];
+    
+    
+    
+    //presetPhoto质量全屏幕显示
+//    [_camera addConsumer:_firstEmptyFilter];
+//
+//    _firstEmptyFilter.contentSize = CGSizeMake(1080, 1440);
+//
+//    CGFloat x = (1440 * 3 / 4.0  - 1080)/2.0;
+//
+//    _firstEmptyFilter.outputFrame = CGRectMake(x, 0,_presentView.contentSize.height * 3 /4.0, _presentView.contentSize.height);
+//
+//    [_firstEmptyFilter addConsumer:_presentView];
+//
+//    [_camera startRunning];
 
 
+
+
+    [_camera addConsumer:_secondEmptyFilter];
+    
+    _secondEmptyFilter.contentSize = CGSizeMake(1080, 1440);
+    
+    CGFloat x = (1440 * 3 / 4.0  - 1080)/2.0;
+    
+    _secondEmptyFilter.outputFrame = CGRectMake(x, 0,_presentView.contentSize.height * 3 /4.0, _presentView.contentSize.height);
+    
+    
+    [_secondEmptyFilter addConsumer:_twoInputFilters];
+    
+    [_secondImageTexture addConsumer:_firstEmptyFilter];
+    
+    _firstEmptyFilter.contentSize = CGSizeMake(640, 640);
+    
+    _firstImageTexture.outputFrame = CGRectMake(0, 0, 1440, 1440);
+    
+    [_firstEmptyFilter addConsumer:_twoInputFilters];
+    
+    [_twoInputFilters addConsumer:_presentView];
+    
+    [_camera startRunning];
+    
+
+    
+
+//    [_firstImageTexture addConsumer:_firstEmptyFilter];
+//
+//    [_firstEmptyFilter addConsumer:_twoInputFilters];
+//
+//    [_secondImageTexture addConsumer:_secondEmptyFilter];
+//
+//    [_secondEmptyFilter addConsumer:_twoInputFilters];
+//
+//    [_twoInputFilters addConsumer:_presentView];
+//
+//
+//    [_secondImageTexture produceAtTime:kCMTimeInvalid];
+//    [_firstImageTexture produceAtTime:kCMTimeInvalid];
+//
+//
+//
+//    UIImage *image = [_firstEmptyFilter imageFromCurrentFrame];
+//    UIImage *image2 = [_secondEmptyFilter imageFromCurrentFrame];
+
+
+}
+
+#pragma mark - Camera Delegate
+-(void)videoCaptor:(SYGLVideoCamera *)videoCaptor willOutputVideoSampleBuffer:(CMSampleBufferRef)sampleBuffer{
+    
+    [_secondImageTexture produceAtTime:kCMTimeInvalid];
+    
+//    [_firstImageTexture produceAtTime:kCMTimeInvalid];
+//    [_secondImageTexture produceAtTime:kCMTimeInvalid];
+    
+
+    
+}
 
 @end
